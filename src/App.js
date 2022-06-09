e1ab024668707af55da5c63c4eae849dd5c17a8a
Jessie Zollinger <jzolly@hotmail.com>
2022-06-09 13:33:36 -0400

    it's working

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
+import {useState} from "react";
 import logo from './logo.svg';
 import './App.css';
 import MovieDisplay from './components/MovieDisplay';
 import Form from './components/Form';
 
 function App() {
+  // API key
+  const API_KEY = "98e3fb1f";
+  // State
+  const [movie, setMovie] = useState(null);
+  // Function to Get Movie
+  const getMovie = async (searchTerm) => {
+    // make fetch request and store response
+    const response = await fetch (
+      `http://www.omdbapi.com/?apikey=${API_KEY}&t=${searchTerm}`
+    );
+    // parse JSON response into a javascript object
+    const data = await response.json();
+    // set the movie sate to the movie
+    setMovie(data);
+  };
+// We pass the getMovie function as a prop called movieSearch
   return (
     <div className="App">
-      <Form />
-      <MovieDisplay />
+      <Form moviesearch={getMovie}/>
+      <MovieDisplay movie={movie}/>
     </div>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,33 @@
+import {useState} from "react";
+
+// Make sure to declare props so you can use them.
 const Form = (props) => {
+    // noun formData, verb setFormData.  Trailing comma
+    const [formData, setFormData] = useState({ 
+        searchterm: "",
+    });
+    // handleChange- updats formData when we type into form
+    const handleChange = (event) => {
+        // use event object to detect key and value to update.  Dynamic property keys
+        setFormData({ ...formData, [event.target.name]: event.target.value});
+    };
+    const handleSubmit = (event) => {
+        //  prevent the page from refreshing on form submission
+        event.preventDefault();
+        // pass the search term to movieSearch prop
+        props.moviesearch(formData.searchterm);
+    };   
+
     return (
         <div>
-            <form>
-                <input type="text" />
-                <input type="submit" name="Search"/>
+            <form onSubmit={handleSubmit}>
+                <input 
+                type="text"
+                name="searchterm" 
+                onChange={handleChange}
+                value={formData.searchterm}
+                />
+                <input type="submit" value="sumbit"/>
             </form>
         </div>
           
