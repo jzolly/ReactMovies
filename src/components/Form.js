e766ff58cb0fc480dd26f130241d9a2b4259106f
Jessie Zollinger <jzolly@hotmail.com>
2022-06-09 09:12:37 -0400

    form started

---
+++ src/components/Form.js
@@ -0,0 +1,13 @@
+const Form = (props) => {
+    return (
+        <div>
+            <form>
+                <input type="text" />
+                <input type="submit" name="Search"/>
+            </form>
+        </div>
+          
+    );
+};
+
+export default Form;
---
+++ src/components/MovieDisplay.js
@@ -0,0 +1,7 @@
+const MovieDisplay = () => {
+    return(
+        <h1>The Movie Display</h1>
+    );
+}
+
+export default MovieDisplay;
